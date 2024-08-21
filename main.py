ffa82586c7f86f04b8267cdbcb1d9d647be279cb
joancolmenares03 <142939033+joancolmenares03@users.noreply.github.com>
2024-08-21 14:17:36 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import funciones
+
+def main():
+    funciones.valorAscendente(test)
+    funciones.comparador(test1,test2)
+    funciones.mezclador(test1,test2)
+
+
+test1 = {
+    1:98,
+    2:97,
+    3:67,
+    4:2
+}
+
+test2 = {
+    1:8,
+    2:97,
+    7:6,
+    3:67,
+    4:89526
+}
+
+if __name__ == "__main__":
+    main()
